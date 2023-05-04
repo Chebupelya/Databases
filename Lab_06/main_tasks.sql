a8368ad204da74e1c96f96815f41ff3528d2dcd1
Nikita <nikitailyin186@gmail.com>
2023-05-04 11:44:00 +0300

    Finished 6th LW

--- Lab_06/main_tasks.sql
+++ Lab_06/main_tasks.sql
@@ -3,16 +3,21 @@ SHOW PARAMETER SPFILE;
 
 --TASK 2
 --Убедился, файл есть.
-
+select name, value from v$parameter where name like 'control_files';
 --TASK 3
 CREATE PFILE = 'INS_PFILE.ORA' FROM SPFILE;
 
 --TASK 4
 SELECT NAME, VALUE FROM V$PARAMETER WHERE NAME = 'open_cursors';
-ALTER SYSTEM SET open_cursors = 350 SCOPE = SPFILE;
+ALTER SYSTEM SET open_cursors = 300;
+alter system set spfile = 'C:\app\Lenovo\product\21c\dbhomeXE\database\INS_SPFILE.ORA';
+
+CREATE SPFILE='C:\app\Lenovo\product\21c\database\SPFILE.ORA' FROM PFILE='C:\app\Lenovo\product\21c\dbhomeXE\database\INS_PFILE.ORA';
+
 
 --TASK 5
-CREATE SPFILE FROM PFILE = 'P1.ORA';
+CREATE SPFILE = 'C:\app\Lenovo\product\21c\dbhomeXE\database\INS_SPFILE.ORA' FROM PFILE = 'INS_PFILE.ORA';
+CREATE SPFILE = 'C:\app\Lenovo\product\21c\database\SPFILEXE1.ORA' FROM PFILE = 'INS_PFILE.ORA';
 
 --TASK 6
 --Запустил
@@ -25,10 +30,14 @@ CREATE SPFILE FROM PFILE = 'P1.ORA';
 SELECT NAME FROM V$CONTROLFILE;
 
 --TASK 9
+ALTER DATABASE BACKUP CONTROLFILE TO TRACE;
+SELECT value FROM v$diag_info;
+
 ALTER SYSTEM SET CONTROL_FILES = 'C:\app\Lenovo\product\21c\oradata\XE\CONTROL01.CTL', 'C:\app\Lenovo\product\21c\oradata\XE\CONTROL02.CTL' SCOPE = SPFILE;
 
 --TASK 10
 --SHOW PARAMETER REMOTE_LOGIN_PASSWORDFILE;
+SELECT * FROM V$PASSWORDFILE_INFO;
 
 --TASK 11
 --Убедился, файл есть.
@@ -41,4 +50,5 @@ SELECT * FROM V$DIAG_INFO;
 --C:\APP\LENOVO\PRODUCT\21C\diag\rdbms\xe\xe\alert\log.xml
 
 --TASK 14
+SELECT * FROM v$diag_info WHERE name = 'Diag Trace';
 --Исследовал
